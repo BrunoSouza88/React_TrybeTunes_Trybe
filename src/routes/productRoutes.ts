09bf8f8e0fed7c3c1dd93dd7dcc1dd48e9064733
BrunoSouza <desouza88@hotmail.com>
2023-05-25 17:52:46 -0300

    Requisito 05 sem testes

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import productController from '../database/controllers/productController';
+import productsValidations from '../middlewares/productsValidations';
 
 const productRouter = Router();
 
-productRouter.post('/products', productController.createProduct);
+productRouter.post(
+  '/products',
+  productsValidations.nameValidations,
+  productsValidations.priceValidations,
+  productController.createProduct,
+);
 productRouter.get('/products', productController.listProducts);
 
 export default productRouter;
